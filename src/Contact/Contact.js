69545ee1394e81a81095d93dc30279d9d735517a
Amarjeet Chauhan <118007811+Amark0347-DevOps@users.noreply.github.com>
2024-12-04 12:51:16 +0530

    Update Contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -52,7 +52,7 @@ const ContactForm = () => {
             required
           ></textarea>
           <button type="submit" className="button">
-            aman
+            ama
           </button>
         </form>
       </div>
